a93ab05bdca16aa96691caefd187db7f48a3f9be
ryanmcfarland14 <40639264+ryanmcfarland14@users.noreply.github.com>
2018-07-27 11:46:18 -0400

    Add files via upload

--- DSHandler.lua
+++ DSHandler.lua
@@ -1,23 +1,92 @@
+--[[
+
+ReplicatedStorage
+	PlayerData
+		Default
+		PlayerList
+			Stats
+			Character
+			Inventory
+	Events
+		DataInput
+			UpdateStats
+			UpdateCharacter
+			UpdateInventory
+			AddCode
+]]
+		
+
+
 --Syntax
-prefix_data = "data_"
-prefix_player = "player_"
+prefix_player = "player_TEST_"
 
 --Keys
-key_playerData = "FRX_datastore&TEST2"
-key_codesData = "FRX_codestore&TEST2"
+key_statsData = "FRX_stats&TEST"
+key_characterData = "FRX_stats&TEST"
+key_inventoryData = "FRX_stats&TEST"
+key_codesData = "FRX_codestore&TEST"
 
 --Service
 local DataStoreService = game:GetService("DataStoreService")
-local DS_main = DataStoreService:GetDataStore(key_playerData)
-local serStorage = game:GetService("ReplicatedStorage")
-local playerFile = serStorage:WaitForChild("PlayerData")
+local repStorage = game:GetService("ReplicatedStorage")
+local playerFile = repStorage:WaitForChild("PlayerData")
 local playerList = playerFile:WaitForChild("PlayerList")
-local codeList = serStorage:WaitForChild("CodeList")
+local codeList = repStorage:WaitForChild("CodeList")
 local defaultFile = playerFile.Default:Clone()
-local remote = serStorage:WaitForChild("Events"):WaitForChild("UpdateValue")
+
+
+--Datastores
+local DS_stats = DataStoreService:GetDataStore(key_statsData)
+local DS_character = DataStoreService:GetDataStore(key_characterData)
+local DS_inventory = DataStoreService:GetDataStore(key_inventoryData)
+local DS_codes = DataStoreService:GetOrderedDataStore(key_codesData)
+
+
+--Remotes
+local eventFolder = repStorage:WaitForChild("Events")
+local eventUpdates = eventFolder.DataInput
+local remote_stats = eventUpdates:WaitForChild("UpdateStats")
+local remote_character = eventUpdates:WaitForChild("UpdateCharacter")
+local remote_inventory = eventUpdates:WaitForChild("UpdateInventory")
+local remote_addCode = eventUpdates:WaitForChild("AddCode")
+
+
+
+--Functions
+function addCode(player, code)
+	DS_codes:SetAsync(prefix_player..player.UserId, code)
+end
+
+function buildCodeList() --OrderedDataStore
+	local pages = DS_codes:GetSortedAsync(false, 10)
+	local currentPageNumber = 0
+	
+	while true do
+		print("Page: "..currentPageNumber)
+		local data = pages:GetCurrentPage()
+		
+		for _, pair in ipairs(data) do
+			print(pair.key..":"..pair.value..)
+			
+			local newVal = Instance.new("StringValue")
+			newVal.Parent = codeList
+			newVal.Name = pair.key
+			newVal.Value = pair.value
+		end
+		
+		if pages.IsFinished then
+			break
+		end
+		
+		pages:AdvanceToNextPageAsync()
+		currentPageNumber = currentPageNumber + 1
+		
+	end
+end
 
 
 --Connections
+--LOAD
 game.Players.PlayerAdded:connect(function(player)
 	
 	print(player.Name.." data loading...")
@@ -28,19 +97,31 @@ game.Players.PlayerAdded:connect(function(player)
 	canSave.Name = "CanSaveData"
 	canSave.Value = true
 	
-	local tStats = defaultFile:Clone()
+	local playerFolder = defaultFile:Clone()
 	
-	local pData
+	local data_stats, data_character, data_inventory
 	
 	--Test for the default data load
 	local DataFetchSuccess, ErrorMessage = pcall(function()
-		pData = DS_main:GetAsync(player.UserId)
+		data_stats = DS_stats:GetAsync(prefix_player..player.UserId)
+		data_character = DS_character:GetAsync(prefix_player..player.UserId)
+		data_inventory = DS_inventory:GetAsync(prefix_player..player.UserId)
 	end)
 		
-	if DataFetchSuccess then	
-		for i, v in next, pData do
-			if tStats:FindFirstChild(i) then
-				tStats[i].Value = v
+	if DataFetchSuccess then
+		for i, v in next, data_stats do
+			if playerFolder.Stats:FindFirstChild(i) then
+				playerFolder.Stats[i].Value = v
+			end
+		end
+		for i, v in next, data_character do
+			if playerFolder.Character:FindFirstChild(i) then
+				playerFolder.Character[i].Value = v
+			end
+		end
+		for i, v in next, data_stats do
+			if playerFolder.Inventory:FindFirstChild(i) then
+				playerFolder.Inventory[i].Value = v
 			end
 		end
 	else	
@@ -48,74 +129,79 @@ game.Players.PlayerAdded:connect(function(player)
 		player:Kick("Your data failed to load. Please rejoin")	
 	end
 
-	tStats.Name = player.UserId
-	tStats.Parent = playerList
+	playerFolder.Name = player.UserId
+	playerFolder.Parent = playerList
+	
+	print(player.Name.."'s data loaded!")
 	
 end)
 
-
+--SAVE
 game.Players.PlayerRemoving:Connect(function(plr)
-	local pStats = playerList:FindFirstChild(plr.UserId)
-	local newData = {}
-	for index, value in next, pStats:GetChildren() do
-		newData[value.Name] = value.Value
+
+	print(player.Name.." data saving...")
+
+	local playerFolder = playerList:FindFirstChild(plr.UserId)
+	local new_stats = {}
+	local new_character = {}
+	local new_inventory = {}
+	for index, value in next, playerFolder.Stats:GetChildren() do
+		new_stats[value.Name] = value.Value
 	end
-	local success, message = pcall(DS_main:SetAsync(plr.UserId, newData))
+	for index, value in next, playerFolder.Character:GetChildren() do
+		new_character[value.Name] = value.Value
+	end
+	for index, value in next, playerFolder.Inventory:GetChildren() do
+		new_inventory[value.Name] = value.Value
+	end
+	local success, message = pcall(	DS_stats:SetAsync(prefix_player..plr.UserId, new_stats);
+									DS_character:SetAsync(prefix_player..plr.UserId, new_character);
+									DS_inventory:SetAsync(prefix_player..plr.UserId, new_inventory))
 	if not success then
 		for i = 1,6 do
 			wait(5)
-			pcall(DS_main:SetAsync(plr.UserId, newData))
+			pcall(	DS_stats:SetAsync(prefix_player..plr.UserId, new_stats);
+					DS_character:SetAsync(prefix_player..plr.UserId, new_character);
+					DS_inventory:SetAsync(prefix_player..plr.UserId, new_inventory))
 		end
 	end
+	
+	print(player.Name.."'s data saved!")
 end)
 
-remote.OnServerEvent:Connect(function(player,valueName,value)
-	
+--UPDATES
+remote_stats.OnServerEvent:Connect(function(player,valueName,value)	
 	local playerFolder = playerList:FindFirstChild(player.UserId)
-	
-	local ref = playerFolder:findFirstChild(valueName)
-	
+	local curentFolder = playerFolder.Stats
+	local ref = currentFolder:findFirstChild(valueName)
 	if ref then
 		ref.Value = value
 	end
-	
 end)
 
-game.Workspace.Part.ClickDetector.MouseClick:connect(function(player)
-	
-	print(player.Name.." data saving...")
-
-	if player.CanSaveData.Value == false then return end
-	
+remote_character.OnServerEvent:Connect(function(player,valueName,value)	
 	local playerFolder = playerList:FindFirstChild(player.UserId)
-	
-	local pStats = playerList:FindFirstChild(player.UserId)
-	local newData = {}
-	for i, v in pairs(playerFolder:GetChildren()) do
-		newData[v.Name] = v.Value
+	local curentFolder = playerFolder.Character
+	local ref = currentFolder:findFirstChild(valueName)
+	if ref then
+		ref.Value = value
 	end
-
-	local DataWriteSuccess,ErrorMessage = pcall(function()
-		DS_main:SetAsync(prefix_player..player.UserId, newData)
-	end)
-	
-	if not DataWriteSuccess then
-	
-		local Retry_Count = 0
-		
-		while Retry_Count<6 do
-			wait(30)
-			local Succeeded, Error = pcall(function()
-				DS_main:SetAsync(prefix_player..player.UserId, newData)
-				print("Saving")
-			end)
-			if Succeeded then break end
-			print("RETRY")
-			Retry_Count = Retry_Count + 1
-		end
-		
-	end
-
 end)
 
+remote_inventory.OnServerEvent:Connect(function(player,valueName,value)	
+	local playerFolder = playerList:FindFirstChild(player.UserId)
+	local curentFolder = playerFolder.Inventory
+	local ref = currentFolder:findFirstChild(valueName)
+	if ref then
+		ref.Value = value
+	end
+end)
+
+remote_addCode.OnServerEvent:Connect(function(player, code)
+	addCode(player, code)
+end)
+
+
+--Startup
+buildCodeList()
 
